41d83813f920d1ca19bf26add5dbdf8465477646
Trex-Codes <juan.13JSG@gmail.com>
2022-12-15 12:37:26 -0500

    Adding first part until BEFORE 2.0 PART with Random letters ASCII in END PART

--- prueba.py
+++ prueba.py
@@ -1,1 +1,5 @@
-from Resources_ASCII.Side_1 import Ev1
+fontEnd_Door = ["slant", "5lineoblique", "bubble", "bulbhead", "contessa", "doom", "eftiwall", "epic", "puffy","rectangles", "shadow", "smkeyboard", "smisome1"]
+
+for i in fontEnd_Door:
+	print(i)
+	
